dabc343f185d73348d059e0c5742b84b5012f6d2
Sebadg34 <Sebadg98@gmail.com>
2021-07-01 04:07:30 -0400

    feat(login): agregada funcionalidad de login y redireccionamiento a pagina de reserva de vuelos

--- ProyectoWebApp_Plat2/Controllers/LoginController.cs
+++ ProyectoWebApp_Plat2/Controllers/LoginController.cs
@@ -5,24 +5,58 @@ using System.Web;
 using System.Web.Mvc;
 using ProyectoWebApp_Plat2.Models;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using Newtonsoft.Json;
+using System.Net.Http.Formatting;
 
 namespace ProyectoWebApp_Plat2.Controllers
 {
     public class LoginController : Controller
     {
+
+
+
         // GET: Login
         public ActionResult Login()
         {
             return View();
         }
 
-
+        
+        
         public ActionResult RegisterCustomer()
         {
             return View();
         }
+        
+        public async Task<ActionResult> PostLogin(string userEmail, string userPassword) {
+
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44350/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+          
+
+            var user = new UserModel() { email = userEmail, contrasenia = userPassword};
+
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/login", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("success");
+            }
+            else {
+
+            }
+           
+            return ToMenu(false,"asd","asd");
+        }
+        
+        
 
         // Métodos que redirigen hacia otra vista
 
@@ -33,7 +67,12 @@ namespace ProyectoWebApp_Plat2.Controllers
         /// <param name="name"></param>
         /// <param name="idUsuario"></param>
         /// <returns>Acción Menu del controlador Home</returns>
-        public ActionResult ToMenu(bool role, string name, int idUsuario)
+        
+
+
+
+        
+        public ActionResult ToMenu(bool role, string name, string idUsuario)
         {
             TempData["Log-In"] = true;
             TempData["Role"] = role;
@@ -48,6 +87,6 @@ namespace ProyectoWebApp_Plat2.Controllers
             return RedirectToAction("RegisterCustomer");
         }
 
-
+        
     }
 }
--- ProyectoWebApp_Plat2/Scripts/CustomJavascript/LoginPost.js
+++ ProyectoWebApp_Plat2/Scripts/CustomJavascript/LoginPost.js
@@ -1,24 +1,115 @@
 ﻿function Login() {
 
     var newUrl = "https://localhost:44350/api/Login";
-
-    $.ajax({
-
-        type: "POST",
-        url: newUrl,
-        dataType: "json",
-        data: {
-            Email: $("#userEmail").val(),
-            Contrasenia: $("#userPassword").val()
-        },
-        success: function (result) {
-            alert("LOGIN EXITOSO\n " + result.Email + " " + result.ID);
-        },
-        error: function (result) {
-            alert("ERROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOR " + result);
-            console.log('Login Fail!!!');
-            console.log(result.Email + result.Contrasenia);
-        }
-    });
+    var rol_id;
+    var rol_nombre;
+    var rol_user;
     
+    $.post(newUrl, { Email: $("#userEmail").val(), Contrasenia: $("#userPassword").val() },
+
+        function (registro, status) {
+
+            var userUrl = "";
+
+            alert("LOGIN EXITOSO\n " + registro.Email + " " + registro.ID);
+
+            // en el caso de ser cliente comun
+            if (registro.ID_Rol == 0) {
+                console.log("CLIENTE ENTRANDO");
+                userUrl = "https://localhost:44350/api/customers/" + registro.ID;
+            }
+            // en el caso de ser administrador 
+            if (registro.ID_Rol == 1) {
+                console.log("ADMIN ENTRANDO");
+                userUrl = "https://localhost:44350/api/managers/" + registro.ID;
+            }
+            
+            $.get(userUrl, function (usuario) {
+
+                     alert(usuario.Nombres);
+                     rol_id = registro.ID_Rol;
+
+                    rol_nombre = usuario.Nombres;
+                rol_user = usuario.ID;
+                
+                window.location.href = "http://localhost:52811/Login/ToMenu?role=" + registro.ID_Rol + "&name=" + usuario.Nombres + "&idUsuario=" + usuario.ID;
+                 //changeView(registro.ID_Rol, usuario.Nombres, usuario.ID);
+                //return { rol_id, rol_nombre, rol_user };
+                //return false;
+            });
+
+
+
+            
+        }).fail(function () {
+            alert("error");
+        });
+    
+   
+
+    //
+    //    $.ajax({
+    //        type: "POST",
+    //        url: newUrl,
+    //        dataType: "json",
+    //        data: {
+    //            Email: $("#userEmail").val(),
+    //            Contrasenia: $("#userPassword").val()
+    //        },
+    //        success: function (rol) {
+    //            alert("LOGIN EXITOSO\n " + rol.Email + " " + rol.ID);
+    //            var userUrl;
+    //            //en el caso de ser cliente
+    //            if (rol.ID_Rol == 0) {
+    //                console.log("CLIENTE ENTRANDO");
+    //                userUrl = "https://localhost:44350/api/customers/" + rol.ID;
+    //
+    //            }
+    //            //en el caso de ser administrador 
+    //            if (rol.ID_Rol == 1) {
+    //                console.log("ADMIN ENTRANDO");
+    //                userUrl = "https://localhost:44350/api/managers/" + rol.ID;
+    //            }
+    //
+    //            $.ajax({
+    //                type: "GET",
+    //                url: userUrl,
+    //                dataType: 'json',
+    //
+    //                success: function (user) {
+    //                    alert(user.Nombres);
+    //                    
+    //                    this.rol_id = rol.ID_Rol;
+    //                    this.rol_nombre = user.Nombres;
+    //                    this.rol_user = user.ID;
+    //
+    //
+    //
+    //                    alert(rol_id);
+    //                    changeView(rol_id, rol_nombre, rol_user);
+    //                }
+    //
+    //
+    //            });
+    //
+    //            
+    //            
+    //        },
+    //        error: function (result) {
+    //            alert("ERROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOR " + result);
+    //            console.log('Login Fail!!!');
+    //            console.log(result.Email + result.Contrasenia);
+    //            
+    //        }
+    //
+    //
+    //    });
+    //    
+   
+}
+
+function changeView(rol, nombre, id) {
+
+    
+   return [ rol, nombre, id ];  
 }

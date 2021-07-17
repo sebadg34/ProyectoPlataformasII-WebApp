08106e9a6ad0534d70bcb6cf169299ceff718001
Vackura <tomas.reimann@gmail.com>
2021-07-17 19:32:12 -0400

    fix: Se han eliminado lineas de codigo innecesarias en el script RegisterPassenger

--- ProyectoWebApp_Plat2/Scripts/CustomJavascript/RegisterPassenger.js
+++ ProyectoWebApp_Plat2/Scripts/CustomJavascript/RegisterPassenger.js
@@ -11,120 +11,79 @@ const expresionesRegulares = {
 }
 
 var campos = [false, false, false, false, false, false, false, false, false];
-var nombreCampos = ["nombre","apellido","email","rut/pasaporte","direccion","numero telefono","nombre emergencia","apellido emergencia","numero telefono emergencia"];
 
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "nombre":
             if (expresionesRegulares.nombre_apellido.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[0] = true;
-                console.log(campos[0]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[0] = false;
-                console.log(campos[0]);
             }
             break;
         case "apellido":
             if (expresionesRegulares.nombre_apellido.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[1] = true;
-                console.log(campos[1]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[1] = false;
-                console.log(campos[1]);
             }
             break;
         case "email":
             if (expresionesRegulares.email.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[2] = true;
-                console.log(campos[2]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[2] = false;
-                console.log(campos[2]);
             }
             break;
         case "rutPasaporte2":
             if ($("#rutPasaporte").val() == "rut") {
                 if (expresionesRegulares.rut.test(e.target.value)) {
-                    console.log(e.target.name + " está bien escrito");
                     campos[3] = true;
-                    console.log(campos[3]);
                 } else {
-                    console.log(e.target.name + "nombre no está bien escrito");
                     campos[3] = false;
-                    console.log(campos[3]);
                 }
             }
             if ($("#rutPasaporte").val() == "numPasaporte") {
                 if (expresionesRegulares.pasaporte.test(e.target.value)) {
-                    console.log(e.target.name + " está bien escrito");
                     campos[3] = true;
-                    console.log(campos[3]);
                 } else {
-                    console.log(e.target.name + "nombre no está bien escrito");
                     campos[3] = false;
-                    console.log(campos[3]);
                 }
             }
             break;
         case "direccion":
             if (expresionesRegulares.direccion.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[4] = true
-                console.log(campos[4]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[4] = false;
-                console.log(campos[4]);
             }
             break;
         case "numeroTelefono":
             if (expresionesRegulares.numeroTelefono.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[5] = true;
-                console.log(campos[5]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[5] = false;
-                console.log(campos[5]);
             }
             break;
         case "nombreEmergencia":
             if (expresionesRegulares.nombre_apellido.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[6] = true;
-                console.log(campos[6]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[6] = false;
-                console.log(campos[6]);
             }
             break;
         case "apellidoEmergencia":
             if (expresionesRegulares.nombre_apellido.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[7] = true;
-                console.log(campos[7]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[7] = false;
-                console.log(campos[7]);
             }
             break;
         case "numeroTelefonoEmergencia":
             if (expresionesRegulares.numeroTelefono.test(e.target.value)) {
-                console.log(e.target.name + " está bien escrito");
                 campos[8] = true;
-                console.log(campos[8]);
             } else {
-                console.log(e.target.name + "nombre no está bien escrito");
                 campos[8] = false;
-                console.log(campos[8]);
             }
             break;
     }
@@ -170,10 +129,8 @@ function reservar() {
 function verificarCampos() {
     for (let i = 0; i < 9; i++) {
         if (!campos[i]) {
-            console.log("verificando campo " + i + " :" + campos[i]);
             return false;
         }
-        console.log("verificando campo " + i + " :" + campos[i]);
     }
     return true;
 }
